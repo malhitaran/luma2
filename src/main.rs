6f2260183c8ac7236f8554ed59589e08143060af
Taranm1 <MalhiTaran182005@gmail.com>
2026-06-18 14:04:43 +0100

    i created a token enum and loop through the source and push the token names, for numbers its slightly differnet we need to keep going as long as there are numbers and less than the amount of characters

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let result = lex("1 + 2");
+    println!("{:?}", result);
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Token{
+    Number(f64),
+    Plus, 
+    Minus, 
+    Star, 
+    Slash, 
+    LParen, 
+    RParen, 
+    Eof
+}
+fn lex(source: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let chars: Vec<char> = source.chars().collect();
+    let mut i = 0;
+
+    while i < chars.len() {
+        let c = chars[i];
+        match c {
+            '+' => { tokens.push(Token::Plus); i += 1; }
+            '-' => { tokens.push(Token::Minus); i += 1; }
+            '*' => { tokens.push(Token::Star); i += 1; }
+            '/' => { tokens.push(Token::Slash); i += 1; }
+            '(' => { tokens.push(Token::LParen); i += 1; }
+            ')' => { tokens.push(Token::RParen); i += 1; }
+            _ if c.is_ascii_digit() => {
+                let mut num_str = String::new();
+                while i < chars.len() && chars[i].is_ascii_digit() {
+                    num_str.push(chars[i]);
+                    i += 1;
+                }
+                let value: f64 = num_str.parse().unwrap();
+                tokens.push(Token::Number(value));
+            }
+            _ => { i += 1; }//this is for anything esle such as whitespaces
+        }
+    }
+
+    tokens.push(Token::Eof);
+    tokens
+}
